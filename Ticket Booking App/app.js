d26a2330aadddcaea5031faf225f71a8e51f1f36
Md. Majidur Rahman Sarker <63114252+sarkermajid@users.noreply.github.com>
2023-01-23 16:10:51 +0600

    Bus ticket booking app

---
+++ Ticket Booking App/app.js
@@ -0,0 +1,249 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      seatStates: {
+        sold: {
+          text: "Sold",
+          color: "#ff0000"
+        },
+        available: {
+          text: "Available",
+          color: "#fff"
+        },
+        booked: {
+          text: "Booked",
+          color: "gray"
+        },
+        selected: {
+          text: "Selected",
+          color: "#00ff00"
+        }
+      },
+
+      seats: [
+        {
+          name: "A1",
+          type: "available",
+          price: 500
+        },
+        {
+          name: "A2",
+          type: "available",
+          price: 500
+        },
+        {
+          name: "A3",
+          type: "available",
+          price: 500
+        },
+        {
+          name: "A4",
+          type: "available",
+          price: 500
+        },
+        {
+          name: "B1",
+          type: "available",
+          price: 450
+        },
+        {
+          name: "B2",
+          type: "available",
+          price: 450
+        },
+        {
+          name: "B3",
+          type: "available",
+          price: 450
+        },
+        {
+          name: "B4",
+          type: "available",
+          price: 450
+        },
+        {
+          name: "C1",
+          type: "sold",
+          price: 500
+        },
+        {
+          name: "C2",
+          type: "sold",
+          price: 500
+        },
+        {
+          name: "C3",
+          type: "sold",
+          price: 500
+        },
+        {
+          name: "C4",
+          type: "sold",
+          price: 500
+        },
+        {
+          name: "D1",
+          type: "available",
+          price: 400
+        },
+        {
+          name: "D2",
+          type: "available",
+          price: 400
+        },
+        {
+          name: "D3",
+          type: "available",
+          price: 400
+        },
+        {
+          name: "D4",
+          type: "available",
+          price: 400
+        },
+        {
+          name: "E1",
+          type: "available",
+          price: 300
+        },
+        {
+          name: "E2",
+          type: "available",
+          price: 300
+        },
+        {
+          name: "E3",
+          type: "booked",
+          price: 300
+        },
+        {
+          name: "E4",
+          type: "booked",
+          price: 300
+        },
+        {
+          name: "F1",
+          type: "available",
+          price: 300
+        },
+        {
+          name: "F2",
+          type: "available",
+          price: 300
+        },
+        {
+          name: "F3",
+          type: "available",
+          price: 300
+        },
+        {
+          name: "F4",
+          type: "available",
+          price: 300
+        }
+      ],
+
+      appliedCoupon: null,
+      couponCode: "",
+      coupons: [
+        {
+          code: "100TAKAOFF",
+          discount: 100
+        },
+        {
+          code: "200TAKAOFF",
+          discount: 200
+        }
+      ],
+
+      name: '',
+      mobile:'',
+      confirmed: false,
+
+    }
+  },
+
+  computed: {
+    selectedSeats() {
+      let selectedSeat = this.seats.filter((item) => item.type === 'selected');
+      return selectedSeat;
+    },
+
+    totalCost() {
+      let totalCost = 0;
+      this.selectedSeats.forEach((seat) => {
+        totalCost += seat.price;
+      });
+
+      if(this.appliedCoupon !== null)
+      {
+        totalCost = totalCost - this.appliedCoupon.discount;
+      }
+      return totalCost;
+    }
+  },
+
+  methods: {
+    handleClick(i) {
+      let clickedSeat = this.seats[i];
+
+      if (clickedSeat.type === 'sold' || clickedSeat.type === 'booked') {
+        alert('You can not select this seat.');
+        return;
+      }
+      if (clickedSeat.type === 'selected') {
+        clickedSeat.type = 'avaliable';
+        return;
+      }
+      if (this.selectedSeats.length > 2) {
+        alert('You can not select more than 3 seats !')
+        return;
+      }
+      clickedSeat.type = 'selected';
+      console.log(clickedSeat);
+    },
+
+    confirm(){
+      if(!this.name || !this.mobile){
+        alert('Please enter your name and mobile');
+        return;
+      }
+
+      this.confirmed = true;
+    },
+
+    resetData() {
+      this.confirmed = false;
+      this.name = "";
+      this.mobile = "";
+      this.appliedCoupon = null;
+    
+      this.seats.forEach((seat) => {
+        if (seat.type === "selected") {
+          seat.type = "sold";
+        }
+      });
+    }
+  },
+
+  watch: {
+    couponCode(newValue){
+      if(newValue.length === 10){
+        let searchedCoupons = this.coupons.filter(
+          (item) => item.code === newValue
+        );
+
+        if(searchedCoupons.length === 1){
+          this.appliedCoupon = searchedCoupons[0];
+          console.log(this.appliedCoupon.code);
+          this.couponCode = '';
+        }else{
+          alert('Coupon not valid!');
+        }
+      }
+    }
+  }
+
+});
+
+app.mount('#app');
